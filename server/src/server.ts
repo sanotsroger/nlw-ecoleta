b5d9b68c1e1791e0786358a9047d0b211f0d6926
Rogério Santos <rogerioantoniodossantos@outlook.com>
2020-06-03 02:16:42 -0300

    Server ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({'status': 'ok'})
-})
+app.use(cors())
+
+app.use(express.json())
+
+app.use(routes)
+
+// Disponibiliza arquivos estáticos
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
